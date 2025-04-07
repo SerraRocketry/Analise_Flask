723895504d6229bdb8b4870ac9135e5dc4caaf07
Gabriel <knichian@gmail.com>
2025-04-06 23:47:55 -0300

    doing a lot of things: fixing static route access, creating "persistence" folder to bind as volume, adding fallback volume configuration to dockerfile, creating docker-compose configuration

--- app/gunicorn.conf.py
+++ app/gunicorn.conf.py
@@ -1,5 +1,18 @@
+import os
+
+watched_files = []
+
+print("")
+print("* Watched:")
+for path in [ "./templates", "./static/css", "./static/js" ]:
+        for file in os.listdir(f"{path}"):
+            print( f"\t{path}/{file}" )
+            watched_files.append( f"{path}/{file}" )
+print("")
 
 bind = "0.0.0.0:8140"
-accesslog = "logs/access.log"
-errorlog = "logs/error.log"
+accesslog = "persistence/logs/access.log"
+errorlog = "persistence/logs/error.log"
 workers = 2
+reload = True
+reload_extra_files = watched_files
